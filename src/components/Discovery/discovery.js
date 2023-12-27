435f2f790cccad1e3c668024ac3c7df6700cf193
Connor Banting <cbanting@vattenar.net>
2023-12-27 01:52:37 -0500

    Post and Discovery components foundation created `

---
+++ src/components/Discovery/discovery.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './discovery.css';
+import Post from '../Post/post.js'
+
+function Discovery() {
+  return (
+    <div>
+        <Post embededURL="https://open.spotify.com/embed/track/7e8utCy2JlSB8dRHKi49xM?utm_source=generator"/>
+        <Post embededURL="https://open.spotify.com/embed/track/1ksVGMm8GlcSWFie5mYPcI?utm_source=generator"/>
+        <Post embededURL="https://open.spotify.com/embed/track/1L94M3KIu7QluZe63g64rv?utm_source=generator"/>
+        <Post embededURL="https://open.spotify.com/embed/track/0bYg9bo50gSsH3LtXe2SQn?utm_source=generator"/>
+        <Post embededURL="https://open.spotify.com/embed/track/5UWwZ5lm5PKu6eKsHAGxOk?utm_source=generator"/>
+        
+     </div>
+  );
+}
+
+export default Discovery;
---
+++ src/components/Post/post.js
@@ -0,0 +1,46 @@
+import './post.css';
+import ironMan from '../Images/ironman.jpeg'
+import Like from '../Images/Like.jpg'
+import Comment from '../Images/Comment.png'
+
+
+function Post({embededURL}) {
+  return (
+    <div className="instagram-post">
+    {/* Top section with username and profile picture */}
+    <div className="user-info">
+      <img
+        src={ironMan} // Replace with the actual profile picture URL
+        alt="Profile Pic"
+        className="profile-pic"
+      />
+      <span className="username">Connorbanting03</span>
+    </div>
+
+    {/* Spotify Embed */}
+    <iframe
+      title="Spotify Embed: Recommendation Playlist"
+      src={embededURL}
+      width="100%"
+      height="360"
+      style={{ minHeight: '360px' }}
+      frameBorder="0"
+      allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+      loading="lazy"
+    />
+
+    {/* Bottom section with likes and comments */}
+    <div className="likes-comments">
+        <button className="likes-button">
+          <img src={Like} alt="Like" />
+        </button>
+        <span className="likes-count"></span>
+        <button className="comments-button">
+          <img src={Comment} alt="Comment" />
+        </button>
+      </div>
+  </div>
+  );
+}
+
+export default Post;
